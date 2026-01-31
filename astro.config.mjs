ccd3b2e1d724a5a1f5fd82f3c58df18d3f767736
hussain <officiallyhussain786@gmail.com>
2026-01-31 19:09:12 +0530

    Add comments system + first blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hussaindev.vercel.app",
+
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
